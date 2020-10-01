7b7269e08506f69c0d9e82f99a3f2d60f801cbf3
dlopezmolinar <dlopezmolinar@miners.utep.edu>
2020-10-01 22:57:08 +0000

    Blood Pressure

--- main.c
+++ main.c
@@ -16,9 +16,31 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
-      printf("Normal\n");
+  if(s > 140) {
+    if(d > 90) {
+      printf("Stage 2 Hypertension\n");
     }
   }
-}
+  if(s < 120){
+    if(d < 80){
+      printf("Normal\n");
+    }  }
+  if(s >180){
+    if(d > 120){
+      printf("Hypertensive Crysis\n");
+    }
+  }
+  if(s >120 && s <129){
+    if(d <80){
+      printf("Elevated\n");
+    }
+  } 
+  if(s >130 && s< 139){
+    if(d >80 && d <89){
+      printf("Stage 1 Hypertension\n");
+    }
+  }
+   }
+
+
+  
